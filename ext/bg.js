f63b2fbc304d4ebb2b6266f941455870e6f13355
Eric Denman <edenman@squareup.com>
2012-07-30 13:48:32 -0700

    Show a page action when a page is being jirafied

--- ext/bg.js
+++ ext/bg.js
@@ -5,8 +5,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         project_keys: localStorage['project_keys'],
         jira_server: localStorage['jira_server']
       });
-    }
-    else {
+    } else {
       sendResponse({}); // snub them.
     }
 });
@@ -20,3 +19,26 @@ function install_notice() {
   chrome.tabs.create({url: "jirafy-config.html"});
 }
 install_notice();
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  if (matchesAnyUrlPatterns(tab.url, localStorage['urls_to_jirafy'])) {
+    chrome.pageAction.show(tabId);
+  }
+};
+
+function matchesAnyUrlPatterns(url, urls_to_jirafy) {
+  urls = urls_to_jirafy.split(",");
+  for(var c = 0; c < urls.length; c++) {
+    if (url.indexOf(urls[c]) != -1) return true;
+  }
+  return false;
+}
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+// When the page action is clicked, go to the config page.
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.update(tab.id, {"url": "jirafy-config.html"});
+});
--- ext/jirafy.js
+++ ext/jirafy.js
@@ -36,14 +36,6 @@ function replaceTicketNumbersWithLinks(projectKeys, jiraServer) {
     });
 }
 
-function matchesAnyUrlPatterns(url, urls_to_jirafy) {
-  urls = urls_to_jirafy.split(",");
-  for(var c = 0; c < urls.length; c++) {
-    if (url.indexOf(urls[c]) != -1) return true;
-  }
-  return false;
-}
-
 chrome.extension.sendRequest({method: "getSettings"}, function(response) {
   url = new String(window.location);
   if (matchesAnyUrlPatterns(url, response.urls_to_jirafy)) {
